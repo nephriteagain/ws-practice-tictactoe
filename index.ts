e21fff172843e3075ae88859a186bea5673ad2e2
nephriteagain <110908855+nephriteagain@users.noreply.github.com>
2023-06-14 19:03:52 +0800

    fixed disconnect not working properly

--- index.ts
+++ index.ts
@@ -31,6 +31,10 @@ type gameObj = Record<string,gameValue>
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send({message: 'connected'})
+})
+
 const { generateClientId } = require('./lib/helper')
 
 // TODO: create a join lobby,
@@ -247,42 +251,56 @@ wss.on('connection', (ws: WebSocket) => {
       })
     }
 
-    // when user closed the tab 
-    if (payload.type === 'disconnect') {
-      const { id, host, guest } = payload
-      if (host||guest) {
-        games.delete(host)
-      }
-      const hostWs = users.get(host)
-      const guestWs = users.get(guest)
-      
-      wss.clients.forEach(client => {
-        if (client === hostWs || client === guestWs) {
-          const gameResponse = { type: 'disconnect', payload: {message: 'a player has disconnected'}}
-          client.send(JSON.stringify(gameResponse))
-        }
-      })
-      users.delete(id)
-      let total = 0
-      for (let item in users) {
-        total++
-      }
-      console.log(`Total users: ${total}`)
-    }
+
 
 
   });  
 
   
   ws.on('close', (code, reason) => {
+    let wsId;
     console.log(code, reason)
     let userCount = 0
     for (let [key,value] of users) {
       userCount++
       if (value == ws) {
+        wsId = key
+
         users.delete(key)        
+        lobby.delete(key)
+        // remove the lobby made by the disconnected user
+        wss.clients.forEach(client => {      
+          let lobbyObj : lobbyObj = {}
+          for (let [key, value] of lobby) {
+            lobbyObj[key] = value
+          }
+          const lobbiesResponse =  { type: 'lobby', payload: lobbyObj}
+          client.send(JSON.stringify(lobbiesResponse))    
+        })
       }
     }
+    
+    // remove game if oppenent is disconnected
+    let gameId;
+    let host;
+    let guest
+    for (let [key,value] of games) {
+      if (value.players.host === wsId || value.players.guest === wsId) {
+        gameId = key
+        games.delete(gameId)
+        host = value.players.host
+        guest = value.players.guest
+      }
+    }
+    const hostWs = users.get(host)
+    const guestWs = users.get(guest)
+    wss.clients.forEach(client => {
+      if (client == hostWs || client == guestWs) {
+        const gameResponse = { type: 'disconnect', payload: {message: 'a player has disconnected'}}
+        client.send(JSON.stringify(gameResponse))
+      }
+    })
+
     console.log(`total User: ${userCount}`)
   })
 
